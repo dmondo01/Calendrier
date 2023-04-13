8974c98b99571037ddb5d91d8703f8a9c9b84ed2
Damien Mondou <dmondo01@univ-lr.fr>
2023-04-13 16:14:29 +0200

    Ajout d'un exemple dans le main

--- src/main.py
+++ src/main.py
@@ -12,6 +12,7 @@ from TeacherType import TeacherType
 
 if __name__ == '__main__':
     #time_table = TimeTable("LOGIN_ULR", 192, TeacherType.EC, datetime(2021, 9, 1), datetime(2022,8,31))
+    # time_table = TimeTable("LOGIN_ULR", 384, TeacherType.PRAG, datetime(2022, 9, 1))
     time_table = TimeTable("LOGIN_ULR", 192, TeacherType.EC, datetime(2022, 9, 1))
     time_table.create_time_table()
     time_table.create_excel_time_table()
